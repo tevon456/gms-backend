7ecfc920ecc44f6c9ad83fadf8cb1e1926ea5d25
Tev <tevondavisjc@gmail.com>
2021-12-10 17:52:35 -0500

    create firebase service

---
+++ api/services/firebase.js
@@ -0,0 +1,21 @@
+const admin = require("firebase-admin");
+
+// get credentials from base64 encoded service account json stored in environment json
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.GOOGLE_CONFIG_BASE64, "base64").toString("ascii")
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+const auth = admin.auth();
+const storage = admin.storage();
+
+module.exports = {
+  db,
+  auth,
+  storage,
+  admin,
+};
