d65c3057de16529c422ff8855fced0c4846f8da5
Tev <tevondavisjc@gmail.com>
2021-12-10 15:44:12 -0500

    initial

---
+++ api/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const httpStatus = require("http-status");
+const helmet = require("helmet");
+const app = express();
+const catchAsync = require("./utils/catchAsync");
+const ApiError = require("./utils/ApiError");
+
+// default options
+app.use(
+  fileUpload({
+    createParentPath: true,
+    limits: {
+      fileSize: 2 * 1024 * 1024 * 1024, //2MB max file(s) size
+    },
+    abortOnLimit: true,
+    responseOnLimit: "Maximum file size is 2mb",
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use(cors());
+
+// set security HTTP headers
+app.use(helmet());
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+});
+
+app.get(
+  "/",
+  catchAsync(async (req, res) => {
+    res.status(httpStatus["200_MESSAGE"]).send({ message: "Hello World" });
+  })
+);
+
+module.exports = app;
