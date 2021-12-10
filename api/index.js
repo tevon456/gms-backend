e9ec2f305053f9b5741fd7697b93d4bb7842842a
Tev <tevondavisjc@gmail.com>
2021-12-10 17:52:19 -0500

    modified entry file

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
+const rateLimit = require("express-rate-limit");
 const httpStatus = require("http-status");
 const helmet = require("helmet");
-const app = express();
 const catchAsync = require("./utils/catchAsync");
 const ApiError = require("./utils/ApiError");
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
 
 // default options
 app.use(
@@ -32,16 +38,27 @@ app.use(express.json());
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
 
+//  apply to all requests
+app.use(limiter);
+
 app.get(
   "/",
   catchAsync(async (req, res) => {
-    res.status(httpStatus["200_MESSAGE"]).send({ message: "Hello World" });
+    res.send({ message: "Hello World" });
   })
 );
+
+app.use("/favicon.ico", (req, res, next) => {
+  res.status(204).end();
+});
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+});
+
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:${3000}`);
 });
 
 module.exports = app;
