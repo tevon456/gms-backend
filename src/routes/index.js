e03271e767389eb2d8694d66fdbe3fd78cf6e1c4
Tev <tevondavisjc@gmail.com>
2021-12-24 00:18:14 -0500

    respond to root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const employeeRoute = require("./employee.route");
 const customerRoute = require("./customer.route");
 
+router.get("/", (req, res) => {
+  res
+    .status(200)
+    .send({
+      message: `${process.env.APP_NAME} ${new Date().toLocaleTimeString()}`,
+    });
+});
 router.use("/employee", employeeRoute);
 router.use("/customer", customerRoute);
 
