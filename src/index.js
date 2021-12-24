7f6d06c5ba6afe907edbd2305a030ab29653e84a
Tev <tevondavisjc@gmail.com>
2021-12-24 01:13:01 -0500

    bug hunt

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const helmet = require("helmet");
 const router = require("./routes");
 const { limiterMiddleware, fileMiddleware } = require("./middleware");
 const app = express();
-let server;
 let port = process.env.PORT || 8000;
 
 app.use(fileMiddleware);
@@ -23,36 +22,10 @@ app.use((req, res) => {
   res.status(400).send({ message: "not found" });
 });
 
-server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(
     `${process.env.APP_NAME.toUpperCase()}-API listening at http://localhost:${port}`
   );
-});
-
-const exitHandler = () => {
-  if (server) {
-    server.close(() => {
-      logger.info("Server closed");
-      process.exit(1);
-    });
-  } else {
-    process.exit(1);
-  }
-};
-
-const unexpectedErrorHandler = (error) => {
-  console.error(error);
-  exitHandler();
-};
-
-process.on("uncaughtException", unexpectedErrorHandler);
-process.on("unhandledRejection", unexpectedErrorHandler);
-
-process.on("SIGTERM", () => {
-  console.info("SIGTERM received");
-  if (server) {
-    server.close();
-  }
 });
 
 module.exports = app;
