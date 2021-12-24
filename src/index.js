d889d439b00aa2d92426942da2bc1346cd195718
Tev <tevondavisjc@gmail.com>
2021-12-24 00:56:32 -0500

    add logs for exceptions and crashes

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const helmet = require("helmet");
 const router = require("./routes");
 const { limiterMiddleware, fileMiddleware } = require("./middleware");
 const app = express();
+let server;
+let port = process.env.PORT || 8000;
 
-// app.use(fileMiddleware);
+app.use(fileMiddleware);
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
@@ -21,11 +23,36 @@ app.use((req, res) => {
   res.status(400).send({ message: "not found" });
 });
 
-const port = process.env.PORT || 8000;
-const server = app.listen(port, () => {
+server = app.listen(port, () => {
   console.log(
     `${process.env.APP_NAME.toUpperCase()}-API listening at http://localhost:${port}`
   );
+});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info("Server closed");
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  console.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+  console.info("SIGTERM received");
+  if (server) {
+    server.close();
+  }
 });
 
 module.exports = app;
