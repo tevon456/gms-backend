4f485091e3bc869a8dfa28c9878db7e2f5f8f5d1
Tev <tevondavisjc@gmail.com>
2021-12-30 22:09:45 -0500

    user update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,6 +27,61 @@ const getAuthenticatedUser = catchAsync(async (req, res) => {
   }
 });
 
+const updateAuthenticatedUser = catchAsync(async (req, res) => {
+  try {
+    let schema = yup.object().shape({
+      first_name: yup.string().required(),
+      last_name: yup.string().required(),
+      email: yup.string().email(),
+    });
+
+    const isValid = await schema.isValid(req.body);
+
+    if (isValid) {
+      const user_token = req.headers?.authorization?.split(" ")[1];
+      const user_decoded = await admin.auth().verifyIdToken(user_token);
+      const employee = await admin
+        .firestore()
+        .collection("employees")
+        .where("uid", "==", user_decoded?.uid)
+        .get();
+
+      let [result] = employee.docs;
+      let employee_update = db.collection("employees").doc(result.id);
+
+      await employee_update.update({
+        ...result.data,
+        email: req.body.email,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        updated_at: new Date().toUTCString(),
+      });
+
+      await admin.auth().updateUser(user_decoded.uid, {
+        displayName: `${req.body.first_name} ${req.body.last_name}`,
+        email: req.body.email,
+      });
+
+      const user = {
+        name: `${req.body.first_name} ${req.body.last_name}`,
+        email: req.body.email,
+        uid: user_decoded?.uid,
+        role: result.data()?.role,
+      };
+
+      res.status(200).send(user);
+    } else {
+      schema.validate(req.body).catch((e) => {
+        res.status(400).send({ message: e.errors });
+      });
+    }
+  } catch (error) {
+    console.log("USER UPDATE: ", error);
+    res.status(400).send({ message: error });
+  }
+});
+
 module.exports = {
   getAuthenticatedUser,
+  updateAuthenticatedUser,
 };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const { authMiddleware } = require("../middleware");
 const { userController } = require("../controllers");
 
-router.route("/").get(userController.getAuthenticatedUser);
+router
+  .route("/")
+  .get(userController.getAuthenticatedUser)
+  .patch(userController.updateAuthenticatedUser);
 module.exports = router;
