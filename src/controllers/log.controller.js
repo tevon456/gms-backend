fd10eb6aadc1160a7ca595c4086f99ab682f7af3
Tev <tevondavisjc@gmail.com>
2022-01-20 16:06:35 -0500

    unused import

--- src/controllers/log.controller.js
+++ src/controllers/log.controller.js
@@ -1,5 +1,5 @@
 const catchAsync = require("../utils/catchAsync");
-const { admin, db } = require("../services/firebase");
+const { admin } = require("../services/firebase");
 const yup = require("yup");
 
 const createLog = catchAsync(async (req, res) => {
